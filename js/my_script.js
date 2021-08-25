ee4e041a4168518019fe91e01e09e4af3c44dede
MorenaFischione <fischione.morena@gmail.com>
2021-08-25 17:27:32 +0200

    sostituzione di "else if"al posto di "if" all'inreno del blocco else

--- js/my_script.js
+++ js/my_script.js
@@ -13,10 +13,10 @@
         if( i % 3 == 0 && i % 5 == 0) {
             console.log("FizzBuzz");
         }
-        if( i % 3 == 0 ) {
+        else if( i % 3 == 0 ) {
             console.log("Fizz");
         }
-        if( i % 5 == 0 ) {
+        else if( i % 5 == 0 ) {
             console.log("Buzz");
         }     
     }
